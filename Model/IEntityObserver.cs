07344d4e631a57c2b6b2a8b97ad2bdca2599cb86
wmaZHnitXu <60194995+wmaZHnitXu@users.noreply.github.com>
2025-05-12 21:12:54 +0300

    Deleted unused

--- Model/IEntityObserver.cs
+++ Model/IEntityObserver.cs
@@ -2,6 +2,7 @@ namespace Core.Model
 {
     public interface IEntityObserver<TObservant> where TObservant : Entity
     {
-        public void ConnectToEntity(TObservant entity);
+        TObservant GetTarget();
+        void ConnectToEntity(TObservant entity);
     }
 }
